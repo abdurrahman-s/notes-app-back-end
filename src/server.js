825fc8384572c8c97fcdc23635b89320cf8d738e
Abdurrahman S Adijaya <abdurrahmans.adijaya@gmail.com>
2023-05-10 04:46:45 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const logger = winston.createLogger({
 const init = async () => {
   const server = Hapi.server({
     port: 5100,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
